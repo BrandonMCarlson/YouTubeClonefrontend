597d1f02160e018e2de2b285cd51bc312f8f52c1
RandallHammond23 <hrandall31@gmail.com>
2021-11-18 21:43:55 -0600

    started cleaning up files. connect to backend. worked on with Brandon and Kenneth.

---
+++ youtube-front/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+
+function App() {
+  const [comments, setComments] = useState([]);
+  const [counter, setCounter] = useState(0);
+  const getAllComments = async () => {
+    await axios.get("http://localhost:5050/api/comments").then((response) => {
+      console.log(response.data);
+      setComments(response.data);
+    });
+  };
+
+  useEffect(() => {
+    getAllComments();
+  }, [counter]);
+  return (
+    <div>
+      <p> {counter}</p>
+      <button onClick={() => setCounter(counter + 1)}> Click me</button>
+    </div>
+  );
+}
+
+export default App;
